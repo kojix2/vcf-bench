57823be4d9e2f203a4dd0981275793ab8ce3fb95
kojix2 <2xijok@gmail.com>
2022-03-29 23:22:26 +0900

    u

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@ hyperfine --warmup 1 --export-markdown result/vcf-bench.md \
   'crystal-htslibcr/read 1kg.chr1.subset.vcf.gz' \
   'rust-htslib/target/release/rust-rhtslib 1kg.chr1.subset.vcf.gz' \
   'python3 python-cyvcf2/read.py 1kg.chr1.subset.vcf.gz' \
+  'python python-pysam/read.py 1kg.chr1.subset.vcf.gz' \
   'c-htslib/read 1kg.chr1.subset.bcf' \
   'crystal-htslibcr/read 1kg.chr1.subset.bcf' \
   'rust-htslib/target/release/rust-rhtslib 1kg.chr1.subset.bcf' \
-  'python python-cyvcf2/read.py 1kg.chr1.subset.bcf'
+  'python python-cyvcf2/read.py 1kg.chr1.subset.bcf' \
+  'python python-pysam/read.py 1kg.chr1.subset.bcf'
