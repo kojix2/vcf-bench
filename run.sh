5e875af390f30c95e8947785feb1dedc55d35fab
kojix2 <2xijok@gmail.com>
2022-03-29 13:01:08 +0900

    Add c-htslib

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 hyperfine --warmup 1 --export-csv vcf-bench.csv \
-  'crystal-htslibcr/read 1kg.chr1.subset.vcf.gz'
+  'c-htslib/read 1kg.chr1.subset.vcf.gz' \
+  'crystal-htslibcr/read 1kg.chr1.subset.vcf.gz' \
+  'c-htslib/read 1kg.chr1.subset.bcf' \
+  'crystal-htslibcr/read 1kg.chr1.subset.bcf' \
