8f0102bd063ca9c7c8405a6f408a2eb001959edb
kojix2 <2xijok@gmail.com>
2022-03-29 13:01:29 +0900

    f

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ hyperfine --warmup 1 --export-csv vcf-bench.csv \
   'c-htslib/read 1kg.chr1.subset.vcf.gz' \
   'crystal-htslibcr/read 1kg.chr1.subset.vcf.gz' \
   'c-htslib/read 1kg.chr1.subset.bcf' \
-  'crystal-htslibcr/read 1kg.chr1.subset.bcf' \
+  'crystal-htslibcr/read 1kg.chr1.subset.bcf'
