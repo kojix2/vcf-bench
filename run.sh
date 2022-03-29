5c5e866d4fccf7eef4dca7a86d9a073b752cdaf0
kojix2 <2xijok@gmail.com>
2022-03-29 15:12:45 +0900

    Fix

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ hyperfine --warmup 1 --export-markdown result/vcf-bench.md \
   'crystal-htslibcr/read 1kg.chr1.subset.vcf.gz' \
   'rust-htslib/target/release/rust-rhtslib 1kg.chr1.subset.vcf.gz' \
   'c-htslib/read 1kg.chr1.subset.bcf' \
-  'crystal-htslibcr/read 1kg.chr1.subset.bcf'
+  'crystal-htslibcr/read 1kg.chr1.subset.bcf' \
   'rust-htslib/target/release/rust-rhtslib 1kg.chr1.subset.bcf'
