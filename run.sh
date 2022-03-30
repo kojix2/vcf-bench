b5ee243b2bb49732d7b719efa584f2332adb2210
kojix2 <2xijok@gmail.com>
2022-03-30 10:34:13 +0900

    Remove go-vcfgo and bcf combination

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 mkdir -p result
+
+#   'go-vcfgo/go-vcfgo                       1kg.chr1.subset.bcf' \
+
 hyperfine --warmup 1 --export-markdown result/vcf-bench.md \
   'c-htslib/read                           1kg.chr1.subset.vcf.gz' \
   'crystal-htslibcr/read                   1kg.chr1.subset.vcf.gz' \
@@ -9,7 +12,6 @@ hyperfine --warmup 1 --export-markdown result/vcf-bench.md \
   'python python-pysam/read.py             1kg.chr1.subset.vcf.gz' \
   'c-htslib/read                           1kg.chr1.subset.bcf' \
   'crystal-htslibcr/read                   1kg.chr1.subset.bcf' \
-  'go-vcfgo/go-vcfgo                       1kg.chr1.subset.bcf' \
   'nim-hts-nim/read                        1kg.chr1.subset.bcf' \
   'rust-htslib/target/release/rust-rhtslib 1kg.chr1.subset.bcf' \
   'python python-cyvcf2/read.py            1kg.chr1.subset.bcf' \
